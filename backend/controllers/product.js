16e021f9f034058db17701a00c8df92cae89d0ab
Emman <192623279+emmanseete@users.noreply.github.com>
2024-12-30 22:06:15 +0800

    Check if product is in database

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -48,6 +48,11 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
     const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({ success: true, message: `Product (${ id }) deleted` });
