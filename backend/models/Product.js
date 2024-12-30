21ea4f77abae41fda9d8578523b4cb391edb23c6
Emman <192623279+emmanseete@users.noreply.github.com>
2024-12-30 20:03:35 +0800

    Add product model using mongoose

---
+++ backend/models/Product.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    // createdAt and updatedAt
+    timestamps: true
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
