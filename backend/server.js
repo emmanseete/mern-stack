fc21b022e22410da9f53fe3b0977cf593359edd7
Emman <192623279+emmanseete@users.noreply.github.com>
2024-12-30 20:31:12 +0800

    Add api -> get products

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.error(`Error: ${ error.message }`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
 app.post("/api/products", async (req, res) => {
     const product = req.body; // user will send this data
 
