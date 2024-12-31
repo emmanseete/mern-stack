029462c6ec2bde356f1cacbfd5eccb9a3351edec
Emman <192623279+emmanseete@users.noreply.github.com>
2024-12-31 16:59:41 +0800

    update default port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import productRoutes from './routes/product.route.js'
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8088
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
